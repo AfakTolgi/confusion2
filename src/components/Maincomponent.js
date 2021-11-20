8012831676fb54bc0c84f8e5782a44e30106eae2
Afak tolgi <afaqtolgi@gmail.com>
2021-11-21 00:01:20 +0530

    react-redux-form revisiited

--- src/components/Maincomponent.js
+++ src/components/Maincomponent.js
@@ -9,7 +9,7 @@ import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import Home from './Homecomponent';
 import Contact from './Contactcomponent';
 import { connect } from 'react-redux';
-
+import { actions } from 'react-redux-form';
 import About from './Aboutuscomponent';
 import { addComment,fetchDishes } from '../redux/ActionCreators';
 
@@ -25,7 +25,8 @@ const mapStateToProps = state =>{
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component{
@@ -74,7 +75,7 @@ class Main extends Component{
           <Route path='/home' component={HomePage}/>
           <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>}/>
           <Route path='/menu/:dishId' component={DishwithId}/>
-          <Route exact path='/contactus' component={Contact}/>
+          <Route exact path='/contactus' component={()=><Contact resetFeedbackForm= {this.props.resetFeedbackForm}/>}/>
           <Route exact path='/aboutus' component={()=><About leaders ={this.props.leaders}/>}/>
           <Redirect to='/home'/>
         </Switch>
